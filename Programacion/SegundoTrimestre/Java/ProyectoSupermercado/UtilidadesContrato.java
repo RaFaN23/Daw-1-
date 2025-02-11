b3a9739255526c494ca9056810d61cafaa37a1cf
Rafael J. Villavicencio Indacochea <am6brafael@gmail.com>
2025-02-11 19:39:12 +0100

    Add files via upload

--- Programacion/SegundoTrimestre/Java/ProyectoSupermercado/UtilidadesContrato.java
+++ Programacion/SegundoTrimestre/Java/ProyectoSupermercado/UtilidadesContrato.java
@@ -1,4 +1,13 @@
 package utilidades;
 
+import com.sun.source.tree.NewArrayTree;
+import modelos.Contrato;
+import modelos.TipoContrato;
+
+import java.util.*;
+
 public class UtilidadesContrato {
+    public Map<TipoContrato, Integer> getNumContratosPorTipo(List<Contrato> contratos){
+        
+    }
 }
